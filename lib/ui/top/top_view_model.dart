03e840d473c27c232b23496e74ba71d2a54c7cda
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2023-04-29 14:42:27 +0900

    feat: create TopViewModel

---
+++ lib/ui/top/top_view_model.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:github_repositories_flutter/domain/model/github_project.dart';
+import 'package:github_repositories_flutter/domain/repository/github_repository.dart';
+import 'package:github_repositories_flutter/ui/top/top_ui_state.dart';
+
+class TopViewModel extends StateNotifier<TopUiState> {
+  final GithubRepository _githubRepository;
+
+  TopViewModel(this._githubRepository) : super(
+      TopUiState(
+          isLoading: true,
+          projects: List<ProjectUiModel>.empty()
+      )
+  );
+
+  void onInitialized() async {
+    try {
+      final List<GithubProject> projects = await _githubRepository.getAllProjects();
+      state = state.onProjectsUpdated(projects);
+    } catch (e) {
+      // TODO Snackbar State的なものを作る
+    }
+  }
+}
