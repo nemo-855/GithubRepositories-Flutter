ced64ffe2e6d20413af2eaee35bf127fafe5db16
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2023-04-29 13:48:57 +0900

    feat: setup di

--- lib/data/api/dio_manager.dart
+++ lib/data/api/dio_manager.dart
@@ -1,4 +1,9 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final dioManagerProvider = Provider<DioManager>((ref) {
+  return DioManager();
+});
 
 class DioManager {
   final dio = Dio();
--- lib/data/api/github_api_impl.dart
+++ lib/data/api/github_api_impl.dart
@@ -1,12 +1,15 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_repositories_flutter/data/api/dio_manager.dart';
 import 'package:github_repositories_flutter/data/api/github_api.dart';
 import 'package:github_repositories_flutter/data/api/response/fetch_project_response.dart';
 import 'package:github_repositories_flutter/data/api/url.dart';
 import 'package:github_repositories_flutter/domain/api_error.dart';
 
+final githubApiProvider = Provider<GithubApi>((ref) => GithubApiImpl(ref.watch(dioManagerProvider)));
+
 class GithubApiImpl implements GithubApi {
   final DioManager _dioManager;
-  GithubApiImpl(this._dioManager)
+  GithubApiImpl(this._dioManager);
 
   @override
   Future<List<FetchProjectResponse>> fetchAllProjects(String userName) async {
