88b618b3ca00e69d5d4e76db527721cae5ded2b4
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2023-05-01 00:31:53 +0900

    feat: TopPageで通信中にローディングを出す処理実装

--- lib/ui/top/top_page.dart
+++ lib/ui/top/top_page.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:github_repositories_flutter/ui/top/top_ui_state.dart';
 import 'package:github_repositories_flutter/ui/top/top_view_model.dart';
 
-class TopPage extends ConsumerWidget {
+class TopPage extends ConsumerStatefulWidget {
   const TopPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final _topViewModel = ref.watch(topViewModelProvider);
+  TopPageState createState() => TopPageState();
+}
 
-    return const Center(
-        child: Text("TopPage")
+class TopPageState extends ConsumerState<TopPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    final topViewModel = ref.read(topViewModelProvider.notifier);
+    topViewModel.onInitialized();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final topViewModel = ref.watch(topViewModelProvider.notifier);
+    final state = ref.watch(topViewModelProvider);
+
+    return Scaffold(
+      body: _buildScaffoldBody(state)
     );
+  }
+  
+  Widget _buildScaffoldBody(TopUiState state) {
+    if (state.isLoading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return const Center(
+        child: Text("TopPage"),
+      );
+    }
   }
 }
