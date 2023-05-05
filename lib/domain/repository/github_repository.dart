5f50db0c99a24f7e195c3023e80a6d5ac6e2488d
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2023-05-05 17:13:34 +0900

    feat: create getSponsors methods

--- lib/domain/repository/github_repository.dart
+++ lib/domain/repository/github_repository.dart
@@ -1,5 +1,7 @@
+import '../model/Sponsore.dart';
 import '../model/github_project.dart';
 
 abstract class GithubRepository {
   Future<List<GithubProject>> getAllProjects();
+  Future<List<Sponsor>> getSponsors();
 }
